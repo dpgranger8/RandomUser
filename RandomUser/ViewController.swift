cb4339445b66e3df06ab04fec39793b4bf04f7fb
David Granger <dpgranger8@gmail.com>
2023-07-14 07:57:32 -0600

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by David Granger on 7/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
